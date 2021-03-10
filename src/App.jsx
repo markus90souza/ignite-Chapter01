7595543f32f1e36e65b0a6c8a296738806db160a
Marcos De Souza <marcosdesouza@MacBook-Air-de-Marcos.local>
2021-03-10 13:59:52 -0300

    Feature - Add css and Sass in project config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import './assets/css/reset.scss';
 
 const App = () => {
-    throw new Error('A casa caiu A festa Acabou');
   return (
-
       <>
 
         <h1>React</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,13 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader',
-            }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+
         ],
     }
 }
